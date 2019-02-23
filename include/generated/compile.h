512c15d2053c0af732f41e44e375e270a985cee6
s.chiotis@mail.ru <s.chiotis@mail.ru>
2019-02-23 21:45:58 +0000

    4.20.11

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Feb 23 18:51:18 UTC 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ArchLinuxTMP"
+#define LINUX_COMPILER "gcc version 8.2.1 20181127 (GCC)"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.20.11-PentelikonRC"
